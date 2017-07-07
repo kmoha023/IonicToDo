fc577271c57c57769d759df42ad9692d8b489d3b
Mohammed <KHAJA_MOHAMMED@homedepot.com>
2017-07-07 16:55:15 -0400

    refactored

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ var cors = require('cors');
 var app = express();
 
 var mongodb = require('mongodb'),
-mongoClient = mongodb.MongoClient,
-ObjectID = mongodb.ObjectID, // Used in API endpoints
-db; // We'll initialize connection below
+    mongoClient = mongodb.MongoClient,
+    ObjectID = mongodb.ObjectID, // Used in API endpoints
+    db; // We'll initialize connection below
+
+var constant = require('./constants/constants');
 
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8080);
@@ -16,22 +18,22 @@ app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cros
 app.use(express.static("www")); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
 
 //Must remove the mongodb://heroku_.... string
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_g4b9k450:8dulqr397pciug9molonpp37lo@ds111262.mlab.com:11262/heroku_g4b9k450";
+var MONGODB_URI = process.env.MONGODB_URI || constant.mongoURI; //"mongodb://heroku_g4b9k450:8dulqr397pciug9molonpp37lo@ds111262.mlab.com:11262/heroku_g4b9k450";
 
 // Initialize database connection and then start the server.
 mongoClient.connect(MONGODB_URI, function (err, database) {
-if (err) {
-process.exit(1);
-}
+    if (err) {
+        process.exit(1);
+    }
 
-db = database; // Our database object from mLab
+    db = database; // Our database object from mLab
 
-console.log("Database connection ready");
+    console.log("Database connection ready");
 
-// Initialize the app.
-app.listen(app.get('port'), function () {
-console.log("You're a wizard, Khaja. I'm a what? Yes, a wizard, on port", app.get('port'));
-});
+    // Initialize the app.
+    app.listen(app.get('port'), function () {
+        console.log("You're a wizard, Khaja. I'm a what? Yes, a wizard, on port", app.get('port'));
+    });
 });
 
 // Todo API Routes Will Go Below
@@ -41,30 +43,30 @@ console.log("You're a wizard, Khaja. I'm a what? Yes, a wizard, on port", app.ge
 */
 
 // GET: retrieve all todos
-app.get("/api/todos", function(req, res) {
-db.collection("todos").find({}).toArray(function(err, docs) {
-if (err) {
-handleError(res, err.message, "Failed to get todos");
-} else {
-res.status(200).json(docs);
-}
-});
+app.get("/api/todos", function (req, res) {
+    db.collection("todos").find({}).toArray(function (err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get todos");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
 });
 
 // POST: create a new todo
-app.post("/api/todos", function(req, res) {
-var newTodo = {
-description: req.body.description,
-isComplete: false
-}
+app.post("/api/todos", function (req, res) {
+    var newTodo = {
+        description: req.body.description,
+        isComplete: false
+    }
 
-db.collection("todos").insertOne(newTodo, function(err, doc) {
-if (err) {
-handleError(res, err.message, "Failed to add todo");
-} else {
-res.status(201).json(doc.ops[0]);
-}
-});
+    db.collection("todos").insertOne(newTodo, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to add todo");
+        } else {
+            res.status(201).json(doc.ops[0]);
+        }
+    });
 });
 
 /*
@@ -72,43 +74,43 @@ res.status(201).json(doc.ops[0]);
 */
 
 // GET: retrieve a todo by id -- Note, not used on front-end
-app.get("/api/todos/:id", function(req, res) {
-db.collection("todos").findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-if (err) {
-handleError(res, err.message, "Failed to get todo by _id");
-} else {
-res.status(200).json(doc);
-}
-});
+app.get("/api/todos/:id", function (req, res) {
+    db.collection("todos").findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get todo by _id");
+        } else {
+            res.status(200).json(doc);
+        }
+    });
 });
 
 // PUT: update a todo by id
-app.put("/api/todos/:id", function(req, res) {
-var updateTodo = req.body;
-delete updateTodo._id;
+app.put("/api/todos/:id", function (req, res) {
+    var updateTodo = req.body;
+    delete updateTodo._id;
 
-db.collection("todos").updateOne({_id: new ObjectID(req.params.id)}, updateTodo, function(err, doc) {
-if (err) {
-handleError(res, err.message, "Failed to update todo");
-} else {
-res.status(204).end();
-}
-});
+    db.collection("todos").updateOne({ _id: new ObjectID(req.params.id) }, updateTodo, function (err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to update todo");
+        } else {
+            res.status(204).end();
+        }
+    });
 });
 
 // DELETE: delete a todo by id
-app.delete("/api/todos/:id", function(req, res) {
-db.collection("todos").deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-if (err) {
-handleError(res, err.message, "Failed to delete todo");
-} else {
-res.status(204).end();
-}
-});
+app.delete("/api/todos/:id", function (req, res) {
+    db.collection("todos").deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+        if (err) {
+            handleError(res, err.message, "Failed to delete todo");
+        } else {
+            res.status(204).end();
+        }
+    });
 });
 
 // Error handler for the api
 function handleError(res, reason, message, code) {
-console.log("API Error: " + reason);
-res.status(code || 500).json({"Error": message});
+    console.log("API Error: " + reason);
+    res.status(code || 500).json({ "Error": message });
 }
